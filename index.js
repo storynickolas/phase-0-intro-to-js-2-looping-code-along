fb11c0c433d0037ad615ec623948847d47a5772e
Nick Story <storynickolasl@gmail.com>
2022-05-03 08:50:13 -0400

    commit tree 329238d7ec0a4f64fe81954e1976de6b7d214e8a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(nameList, event) {
+  let writtenCards = []
+  for(let i=0; i < nameList.length; i++) {
+    writtenCards.push(`Thank you, ${nameList[i]}, for the wonderful ${event} gift!`)
+  }
+  return writtenCards;
+}
+
+function countDown(i) {
+  while(i >= 0) {
+    console.log(i)
+    i--
+  }
+}
